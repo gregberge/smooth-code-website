fb9ffbb6afd0bd4b6137cf72a4a96cff41c94002
Greg Bergé <neoziro@gmail.com>
2014-11-03 17:14:17 +0100

    Optimize performances.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var express = require('express');
+var compress = require('compression');
 
 var app = express();
 var server = http.createServer(app);
 
+app.use(compress());
 app.use(express.static('./build'));
 
 server.listen(process.env.PORT || 8080);
